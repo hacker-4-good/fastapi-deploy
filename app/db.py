ae4d032df9615125a5d4d7fe9574f6ff37a9f170
Mayank Goswami <mayankhacker04@gmail.com>
2026-03-20 13:56:20 +0530

    refactor MongoDB client initialization and update requirements to include certifi and ssl

--- app/db.py
+++ app/db.py
@@ -1,7 +1,16 @@
 from pymongo import MongoClient
 from app.config import MONGO_URI
+import certifi 
+import ssl
 
-client = MongoClient(MONGO_URI, tls=True, tlsAllowInvalidCertificates=True)
+client = MongoClient(
+    MONGO_URI,
+    tls=True,
+    tlsCAFile=certifi.where(),
+    tlsAllowInvalidCertificates=True,
+    ssl_cert_reqs=ssl.CERT_NONE
+)
+
 db = client["gps_tracker"]
 
 users = db["users"]
